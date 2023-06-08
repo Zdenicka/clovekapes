3e05ebceb906c94adb679098902e6eabd2e6bf51
Birma <birma@seznam.cz>
2023-06-08 20:44:12 +0200

    pořád to nefunguje podle představ

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,40 @@
 <template>
-  <div class="q-pa-sm">
+  <div class= "full-width row wrap justify-start items-stretch content-start">
+      <div class= "col-12 q-ma-lg " style="overflow: auto; bg-color: primary ">
 
-      <q-layout
-        view="hhh Lpr fff"
-        class="shadow-2 rounded-borders"
-      >
-        <q-header elevated >
-          <q-toolbar >
-            <q-btn flat @click="drawerLeft = !drawerLeft" round dense icon="menu" />
-            <p></p>
-            <p></p>
-            <q-toolbar-title>Člověk a pes - spolu</q-toolbar-title>
-          </q-toolbar>
-        </q-header>
+          <q-btn round color="primary" icon="menu" />
+          <div class="text-subtitle1 text-center">
+            Člověk a pes - spolu
+          </div>
+      </div>
 
-        <q-footer>
-          <q-toolbar>
-            <q-toolbar-title>Footer</q-toolbar-title>
-          </q-toolbar>
-        </q-footer>
+      <div class="col-3 q-gutter-md q-pa-lq" style="overflow: auto;">
+        <q-item active clickable v-ripple>
+          <router-link to="/">Home</router-link>
+        </q-item>
+        
+        <q-item active clickable v-ripple>
+          <router-link to="/treninky">Treninky</router-link>
+        </q-item>
 
-        <q-drawer
-          v-model="drawerLeft"
-          :breakpoint="700"
-          elevated
-        >
-          <q-scroll-area class="fit">
-            <div class="q-pa-sm">
-              <q-list padding class="menu-list">
+        <q-item active clickable v-ripple>
+          <router-link to="/prihlasovani">Přihlašování na tréninky</router-link>
+        </q-item>
 
-            
-                <q-item active clickable v-ripple>
-                  <router-link to="/">Home</router-link>
-                </q-item>
-                
-                <q-item active clickable v-ripple>
-                  <router-link to="/treninky">Treninky</router-link>
-                </q-item>
+        <q-item active clickable v-ripple>
+          <router-link to="/kontakt">Kontakt</router-link>
+        </q-item>
+      </div>
 
-                <q-item active clickable v-ripple>
-                  <router-link to="/prihlasovani">Přihlašování na tréninky</router-link>
-                </q-item>
+      <div class="col-9 q-gutter-md q-pa-xl" style="overflow: auto;">
+        <router-view/>
+      </div>
+      
+      <div class= "col-12" style="overflow: auto bg-color: $primary">
+      </div>
 
-                <q-item active clickable v-ripple>
-                  <router-link to="/kontakt">Kontakt</router-link>
-                </q-item>
-
-              </q-list>
-            </div>
-          </q-scroll-area>
-        </q-drawer>
-
-        <q-page-container>
-          <q-page style="padding-top: 60px" class="col-8">
-            <router-view/>
-          </q-page>
-
-          <q-page-scroller position="bottom">
-            <q-btn fab icon="keyboard_arrow_up" color="red" />
-          </q-page-scroller>
-        </q-page-container>
-      </q-layout>
-    </div>
-
+      
+  </div>
 </template>
 
 <script>
