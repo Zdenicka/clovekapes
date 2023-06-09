77f81749e2cfd6b655ad7528b836cf4200e7a68b
Birma <birma@seznam.cz>
2023-06-09 20:30:57 +0200

    větev grid

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
 <template>
-  <div class= "full-width row wrap justify-start items-stretch content-start">
-      <div class= "col-12 q-ma-lg " style="overflow: auto; bg-color: primary ">
+    <div class= "full-width row wrap justify-evenly items-stretch content-stretch" style="background-color: grey; height: 100vh;">
+        <div class= "col-12 q-ma-xs" style= "background-color: white; height: 20%">
 
-          <q-btn round color="primary" icon="menu" />
-          <div class="text-subtitle1 text-center">
-            Člověk a pes - spolu
-          </div>
-      </div>
+            <q-btn round color="primary" icon="menu" />
+            <div class="text-subtitle1 text-center">
+              Člověk a pes - spolu
+            </div>
+        </div>
 
-      <div class="col-3 q-gutter-md q-pa-lq" style="overflow: auto;">
-        <q-item active clickable v-ripple>
-          <router-link to="/">Home</router-link>
-        </q-item>
+        <div class="col-2 q-pt-xl" style="background-color: white; border-radius: 20px">
+          <q-item active clickable v-ripple>
+            <router-link to="/">Home</router-link>
+          </q-item>
+          
+          <q-item active clickable v-ripple>
+            <router-link to="/treninky">Treninky</router-link>
+          </q-item>
+
+          <q-item active clickable v-ripple>
+            <router-link to="/prihlasovani">Přihlašování na tréninky</router-link>
+          </q-item>
+
+          <q-item active clickable v-ripple>
+            <router-link to="/kontakt">Kontakt</router-link>
+          </q-item>
+        </div>
+
+        <div class="col-9 q-pa-xl" style="background-color: white; min-height: 70%; border-radius: 20px">
+          <router-view/>
+        </div>
         
-        <q-item active clickable v-ripple>
-          <router-link to="/treninky">Treninky</router-link>
-        </q-item>
+        <div class= "col-12" style="background-color: white; min-height: 5%">
+        </div>
 
-        <q-item active clickable v-ripple>
-          <router-link to="/prihlasovani">Přihlašování na tréninky</router-link>
-        </q-item>
+    </div>    
 
-        <q-item active clickable v-ripple>
-          <router-link to="/kontakt">Kontakt</router-link>
-        </q-item>
-      </div>
-
-      <div class="col-9 q-gutter-md q-pa-xl" style="overflow: auto;">
-        <router-view/>
-      </div>
-      
-      <div class= "col-12" style="overflow: auto bg-color: $primary">
-      </div>
-
-      
-  </div>
 </template>
 
 <script>
