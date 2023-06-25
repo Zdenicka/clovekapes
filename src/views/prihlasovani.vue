6a729642f239ca7e35792eeabad661a5fab3be89
Birma <birma@seznam.cz>
2023-06-25 21:12:28 +0200

    oprava

--- src/views/prihlasovani.vue
+++ src/views/prihlasovani.vue
@@ -4,7 +4,7 @@
     <p>Individuální tréninky po domluvě co chcete řešit - jsou možné venku nebo přímo u vás doma.</p>
     <p>Cena individuálního tréninku je 500 Kč/h a pak 100 Kč/ každá započatá čtvrthodina. V oblasti mezi Nymburkem, Jičínem a Mladou Boleslaví si nepočítám cestu, delší vzdálenosti záleží na domluvě.</p>
   <h5>Skupinové tréninky a socializační procházky v Jičíně a Mladé Boleslavi</h5>
-    <p>Vypisuji spíše sporadicky. Pokud jsou, najkdete je na <a href="https://clovek-a-pes-spolu.dogres.cz/trainings" target="_blank">mém dogresu.</a></p>
+    <p>Vypisuji spíše sporadicky. Pokud jsou, najdete je na <a href="https://clovek-a-pes-spolu.dogres.cz/trainings" target="_blank">mém dogresu.</a></p>
     <p>Pokud vypsané nejsou a měli byste o skupinku zájem, <router-link to="/kontakt">napište mi</router-link>, něco vymyslíme :)</p>
   <h5>ZKO Nymburk</h5>
     <p>Na tréninky v rámci <a href="https://zkonymburk.blogspot.com/">ZKO Nymburk</a> se přihlašujte přes <a href="https://zko-nymburk.dogres.cz/trainings?trainer_id=924" target="_blank">dogres ZKO</a>. 
