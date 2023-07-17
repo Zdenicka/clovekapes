15b782892153792bd8532535123b30f69b4ece53
Birma <birma@seznam.cz>
2023-07-17 17:43:04 +0200

    úprava - validace amp

--- src/main.js
+++ src/main.js
@@ -5,4 +5,11 @@ import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
 import { createMetaManager } from 'vue-meta'
 
+router.afterEach((to) => {
+    const canonicalLink = document.querySelector('link[rel="canonical"]')
+    if (canonicalLink) {
+      canonicalLink.href = "https://pes.academy" + to.fullPath
+    }
+  })
+
 createApp(App).use(Quasar, quasarUserOptions).use(router).use(createMetaManager()).mount('#app')
