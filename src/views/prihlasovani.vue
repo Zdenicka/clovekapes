c1f6a48844e2ea9e3d3f922d0fcb2a77486dad53
Birma <birma@seznam.cz>
2024-01-08 16:16:43 +0100

    změny v kurzech

--- src/views/prihlasovani.vue
+++ src/views/prihlasovani.vue
@@ -8,22 +8,26 @@
   <h5>Skupinové tréninky a socializační procházky v Jičíně a Mladé Boleslavi</h5>
     <p>Vypisuji spíše sporadicky. Pokud jsou, najdete je na <a href="https://clovek-a-pes-spolu.dogres.cz/trainings" target="_blank">mém dogresu.</a></p>
     <p>Pokud vypsané nejsou a měli byste o skupinku zájem, <router-link to="/kontakt">napište mi</router-link>, něco vymyslíme :)</p>
-  <h5>Mladá Boleslav - kurzy</h5>
+  <!-- <h5>Mladá Boleslav - kurzy</h5>
   <p>První běh kurzů v Mladé Boleslavi začne na přelomu října a listopadu. Přihlašování je přes <a href="https://clovek-a-pes-spolu.dogres.cz/trainings" target="_blank">můj dogres</a> 
   a hlásíte se na první tréninkovou lekci. Týden před ní proběhne ještě úvodní teoretická lekce - určená pro školku a základy rodinného psa, nicméně vy, kdo se přihlásíte na přivolávače, si ji určitě můžete také přijít poslechnout :).</p>
   <p>Přihlásit se můžete na následující kurzy:</p>
   <p><a href="https://clovek-a-pes-spolu.dogres.cz/trainings/249544" target="_blank">Školka pro štěňátka</a></p>
   <p><a href="https://clovek-a-pes-spolu.dogres.cz/trainings/249556" target="_blank">Základy rodinného psa pro dospělé psy</a></p>
-  <p><a href="https://clovek-a-pes-spolu.dogres.cz/trainings/249565" target="_blank">Přivolávači</a></p>
+  <p><a href="https://clovek-a-pes-spolu.dogres.cz/trainings/249565" target="_blank">Přivolávači</a></p>-->
 
   <h5>ZKO Nymburk - kurzy</h5>
-    <p>Nejbližší běh kurzů odstartuje v Nymburce na přelomu října a listopadu. Přihlašování je přes <a href="https://zko-nymburk.dogres.cz/trainings?trainer_id=924" target="_blank">dogres ZKO</a> a hlásíte se na první tréninkovou lekci. Týden před ní proběhne ještě úvodní teoretická lekce. Kurzy na rozdíl od jednotlivých lekcí nejsou hrazeny kreditovým systémem</p>
-    <p><a href="https://zko-nymburk.dogres.cz/trainings/232968" target="_blank">Přihlašování Ošetření se souhlasem</a></p>
-    <p><a href="https://zko-nymburk.dogres.cz/trainings/251028" target="_blank">Přihlašování Všeobecná sportovní přípravka</a></p>
-    <p><a href="https://zko-nymburk.dogres.cz/trainings/249050" target="_blank">Přihlašování Základy rodinného psa pro dospělé psy</a></p>
+    <p>Nejbližší běh kurzů odstartuje v Nymburce na přelomu února a března 2024. Přihlašování je přes <a href="https://zko-nymburk.dogres.cz/trainings?trainer_id=924" target="_blank">dogres ZKO</a> a hlásíte se na první tréninkovou lekci. Týden před ní proběhne ještě úvodní teoretická lekce. Kurzy na rozdíl od jednotlivých lekcí nejsou hrazeny kreditovým systémem</p>
+    <!--<p><a href="https://zko-nymburk.dogres.cz/trainings/232968" target="_blank">Přihlašování Ošetření se souhlasem</a></p>-->
+    <p><a href="https://zko-nymburk.dogres.cz/trainings/268349" target="_blank">Přihlašování Všeobecná sportovní přípravka</a></p>
+    <p><a href="https://zko-nymburk.dogres.cz/trainings/268337" target="_blank">Přihlašování Základy rodinného psa pro&nbsp;dospělé psy a puberťáky</a></p>
+    <p><a href="https://zko-nymburk.dogres.cz/trainings/268343" target="_blank">Přihlašování Základy rodinného psa pro&nbsp;štěňátka</a></p>
+    <p><a href="https://zko-nymburk.dogres.cz/trainings/274451" target="_blank">Přihlašování Pro&nbsp;psí důchodce a/nebo citlivky</a></p>
+    <p><a href="https://zko-nymburk.dogres.cz/trainings/274433" target="_blank">Přihlašování Z&nbsp;bojínka hrdinou</a></p>
+    <p><a href="https://zko-nymburk.dogres.cz/trainings/274445" target="_blank">Přihlašování Pro&nbsp;reaktivce - s&nbsp;ostatními psy a lidmi v&nbsp;pohodě</a></p>
   <h5>ZKO Nymburk - jednotlivé tréninky</h5>
     <p>Na tréninky v rámci <a href="https://zkonymburk.blogspot.com/" target="_blank">ZKO Nymburk</a> se přihlašujte přes <a href="https://zko-nymburk.dogres.cz/trainings?trainer_id=924" target="_blank">dogres ZKO</a>. 
-    Na cvičáku funguje kreditový systém, pro přihlášení je tedy nutné mít zakoupené kredity.</p>
+    Na&nbsp;cvičáku funguje kreditový systém, pro přihlášení je tedy nutné mít zakoupené kredity.</p>
     <iframe src="https://zko-nymburk.dogres.cz/trainings?trainer_id=924" width="100%" height="400px"></iframe>
 
 
--- src/views/treninky.vue
+++ src/views/treninky.vue
@@ -10,22 +10,28 @@
     <h5>Skupinové tréninky</h5>
         <p>Skupinové tréninky v současné době nabízím hlavně v&nbsp;<a href="https://zkonymburk.blogspot.com/" target="_blank">ZKO Nymburk</a>, nicméně občas je vypíšu i v&nbsp;Mladé Boleslavi nebo Jičíně.</p>
     <h3>Kurzy</h3>
-        <p>Od začátku podzimu běží mé kurzy v&nbsp;<a href="https://zkonymburk.blogspot.com/" target="_blank">ZKO Nymburk</a> a už je spuštěno i přihlašování na kurzy v Mladé Boleslavi. Tyto kurzy obsahují i teoretický úvod, který není pro účastníky povinný, zato je velmi užitečný.</p>
+        <p>Od začátku podzimu běží mé kurzy v&nbsp;<a href="https://zkonymburk.blogspot.com/" target="_blank">ZKO Nymburk</a>. Tyto kurzy obsahují i teoretický úvod, který není pro účastníky povinný, zato je velmi užitečný.</p>
         <p>Cílem všech mých kurzů je usnadnit vám společný život s vaším psem nebo nastartovat vaši společnou sportovní kariéru.</p>
-        <p>Za obrovskou výhodu kurzů považuji to, že tréninky na sebe navazují, postupujete v nich od jednodušších věcí ke složitějším a pokud se někde objeví zádrhel, snadno se k němu na příští lekci vrátíme a odstraníme ho. Máte také možsnost sledovat pokroky ostatních týmů a učit se od nich.
-        Celý postup tak dává větší smysl a po celou dobu kurzy (i po něm) můžete stavět na teoretickém základu, ktterý získáte na úvodní hodině.</p>
-    <h5>Základy rodinného psa pro dospělé psy</h5>
+        <p>Za obrovskou výhodu kurzů považuji to, že&nbsp;tréninky na sebe navazují, postupujete v nich od jednodušších věcí ke složitějším a pokud se někde objeví zádrhel, snadno se k&nbsp;němu na příští lekci vrátíme a odstraníme ho. Máte také možsnost sledovat pokroky ostatních týmů a učit se od&nbsp;nich.
+        Celý postup tak dává větší smysl a po&nbsp;celou dobu kurzy (i po&nbsp;něm) můžete stavět na&nbsp;teoretickém základu, ktterý získáte na&nbsp;úvodní hodině.</p>
+    <h5>Základy rodinného psa pro dospělé psy a puberťáky</h5>
         <p>Kurz vhodný pro všechny psíky, u&nbsp;kterých máte pocit, že byste si v&nbsp;běžných situacích mohli lépe rozumět. Možná jste v jejich štěněcím období neměli dostatek času, nebo vám vstoupil do&nbsp;cesty běžný život, 
         možná jste neměli dost podpory a informací, nebo jste si je přivedli domů už dospělé či dospívající. Nevadí, i dospělého psa slušnému chování naučíme, nebojte. V&nbsp;klidu a po&nbsp;dobrém.</p>
+    <h5>Základy rodinného psa pro dospělé psy a puberťáky</h5>
+        <p>V&nbsp;tomto kurzu je stejná náplň, jako v Základech rodinného psa pro&nbsp;dospělé psy a puberťáky. Více se však přizpůsobujeme teprve se vyvíjecímu mozku štěňátek - jejich větší potřebě zkoumat prostředí, kratší schopnosti se soustředit i rychleji nastupující únavě.</p>
     <h5>Všeobecná sportovní přípravka</h5>
         <p>Tady předpokládám, že si se&nbsp;svým psíkem docela rozumíte a máte nějaké sportovní ambice. Ambicí může být i (jen) pravidelné ježdění na tréninky nějakého sportu a vybití nastřádané energie. Nebo možná už i tréninky nějakého sportu navštěvujete a zjistili jste, že by bylo fajn, aby váš psí parťák uměl věci, které nejsou úplně předmětem onoho sportu, ale vám to tréninky ztěžuje a váš trenér nemá čas, aby vám s tím během úzce zaměřených tréninků poradil.
         O&nbsp;co například může jít? Takové klidné čekání, než na&nbsp;vás přijde řada, bývá častým kamenem úrazu. Zapracujeme na&nbsp;motivaci, sebekontrole a podíváme se na&nbsp;základní chování, které se vám bude hodit snad u&nbsp;všech psích sportů :) </p>
     <h5>Ošetření se souhlasem</h5>
         <p>Kurz pro všechny psí citlivky i macha, se kterými možná doma bojujete při&nbsp;vyndavání klíšťat, stříhání drápků, česání nebo kontrole uší. Možná váš problém začíná až při&nbsp;pohledu na dveře veteriny. Nebojte, i to se dá trpělivou prací změnit.</p>
-    <h5>Školka pro štěňátka</h5>
-        <p>Stejný koncept jako Základy rodinného psa pro dospělé psy, jen přizpůsobená štěňátkům - jejich větší potřebě zkoumat prostředí, kratší schopnosti se soustředit i rychleji nastupující únavě</p>
+    <h5>Pro&nbsp;psí důchodce a/nebo citlivky</h5>
+        <p>Tady se zaměříme hlavně na udržení/rozvoj motoriky. Budeme hrát různé čuchací hry, řešit hlavolamy, šlapat po nerovných površích. Prostě se budeme věnovat věcem, které přinášejí dobrý pocit, zlepšují náladu, udržují mozek i tělo v běhu a nedovolí jim zakrnět a zároveň zvyšují sebedůveru psíka</p>
+    <h5>Z&nbsp;bojínka hrdinou</h5>
+        <p>Budeme hrát hry zvyšující sebevědomí. Budeme trénovat strategie, jak našemu psíkovi pomoci v situacích, které jsou pro něj obtížné, jak potkávat a míjet děsivé lidi nebo psy. Budeme se zabývat i strachem ze zvuků nebo divných předmětů.</p>
+    <h5>Pro&nbsp;reaktivce - s&nbsp;ostatními psy a lidmi v&nbsp;pohodě</h5>
+        <p>Kurz pro psíky reagující přehnaně na podněty - budeme je učit, jak tyto situace lépe zvládat a vy se naučíte, jak je podpořit a pomoci jim.</p>
     <h5>Přivolávači</h5>
-        <p>Hry na přivolání, soustředění a sebekontrolu - od Susan Garrett i jiných.</p>
+        <p>Hry na přivolání, soustředění a sebekontrolu - od&nbsp;Susan Garrett i jiných.</p>
 </template>
 <script>
 import { useMeta } from 'vue-meta'
